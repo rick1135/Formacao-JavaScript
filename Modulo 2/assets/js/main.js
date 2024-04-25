cc02226b05e61ba04f74cb892b424bb29448a700
Alberto Henrique <rick1135@hotmail.com>
2024-04-25 18:45:31 -0300

    studying

--- Modulo 2/assets/js/main.js
+++ Modulo 2/assets/js/main.js
@@ -1,1 +1,15 @@
-console.log('Sucesso!')
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+fetch(url).then(function (response){
+    response.json().then(function(responseBody){
+        console.log(responseBody)
+    })
+}).catch(function (error){
+    console.error(error)
+}).finally(function(){
+    console.log('finally')
+})
+
+const x = 10+10;
+console.log(x);
