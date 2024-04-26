03e4f659f31fa1049bf636547ddcaefa686fcb13
Alberto Henrique <rick1135@hotmail.com>
2024-04-26 20:00:49 -0300

    studying

--- Modulo 2/assets/js/main.js
+++ Modulo 2/assets/js/main.js
@@ -1,15 +1,21 @@
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-fetch(url).then(function (response){
-    response.json().then(function(responseBody){
-        console.log(responseBody)
-    })
-}).catch(function (error){
-    console.error(error)
-}).finally(function(){
-    console.log('finally')
-})
+function convertPokemonToLi(pokemon) {
+  return `<li class="pokemon ${pokemon.type}">
+    <span class="number">#00${pokemon.number}</span>
+    <span class="name">${pokemon.name}</span>
+    <div class="detail">
+      <ol class="types">
+        ${pokemon.types.map((type => `<li class="type">${type}</li>`)).join('')}
+      </ol>
+      <img
+        src="${pokemon.photo}"
+        alt="${pokemon.name}"
+      />
+    </div>
+  </li>`;
+}
 
-const x = 10+10;
-console.log(x);
+const pokemonList = document.getElementById("pokemonList");
+
+pokeApi.getPokemons().then((pokemons = []) => {
+  pokemonList.innerHTML = pokemons.map(convertPokemonToLi).join("");
+});
