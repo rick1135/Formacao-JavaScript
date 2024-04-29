1540e2ff2664eb0fe426a0c9ccca9772aaff549b
Alberto Henrique <rick1135@hotmail.com>
2024-04-29 18:41:43 -0300

    desafio

--- Modulo 2/assets/js/main.js
+++ Modulo 2/assets/js/main.js
@@ -1,5 +1,6 @@
 const pokemonList = document.getElementById("pokemonList");
 const loadMoreButton = document.getElementById("loadMore");
+const pokemonListDetails = document.getElementById("pokemonListDetails");
 
 const maxPokemon = 151;
 const limit = 5;
@@ -11,20 +12,23 @@ function loadPokemonItens(offset, limit) {
       .map(
         (pokemon) =>
           `<li class="pokemon ${pokemon.type}">
-    <span class="number">#00${pokemon.number}</span>
-    <span class="name">${pokemon.name}</span>
-    <div class="detail">
-      <ol class="types">
-        ${pokemon.types
-          .map((type) => `<li class="type ${pokemon.type}">${type}</li>`)
-          .join("")}
-      </ol>
-      <img
-        src="${pokemon.photo}"
-        alt="${pokemon.name}"
-      />
-    </div>
-  </li>`
+            <span class="number">#00${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+              <ol class="types">
+                ${pokemon.types
+                  .map(
+                    (type) => `<li class="type ${pokemon.type}">${type}</li>`
+                  )
+                  .join("")}
+                  <button type="button" id="moreDetails">More</button>
+              </ol>
+              <img
+                src="${pokemon.photo}"
+                alt="${pokemon.name}"
+              />
+            </div>
+          </li>`
       )
       .join("");
   });
@@ -40,8 +44,13 @@ loadMoreButton.addEventListener("click", () => {
     const newLimit = maxPokemon - offset;
     loadPokemonItens(offset, newLimit);
     loadMoreButton.parentElement.removeChild(loadMoreButton);
-  }
-  else{
+  } else {
     loadPokemonItens(offset, limit);
+  }
+});
+
+pokemonList.addEventListener('click', (event) => {
+  if (event.target.matches('#moreDetails')) {
+    window.location.assign('details.html');
   }
 });
