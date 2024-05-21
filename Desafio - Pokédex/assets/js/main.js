dc23f1205e800030862ad189d29867a27319578f
Alberto Henrique <rick1135@hotmail.com>
2024-05-21 18:18:13 -0300

    studying

--- Desafio - Pokédex/assets/js/main.js
+++ Desafio - Pokédex/assets/js/main.js
@@ -1,8 +1,28 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody)=> console.log(jsonBody))
-    .catch((error) => console.log(error))
+function convertPokemonToLi(pokeon) {
+  return `<ol class="pokemons">
+    <li class="pokemon">
+      <span class="number">#001</span>
+      <span class="name">${pokeon.name}</span>
+      <div class="detail">
+        <ol class="types">
+            <li class="type">grass</li>
+            <li class="type">poison</li>
+        </ol>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+          alt="${pokeon.name}"
+        />
+      </div>
+    </li>
+  </ol>`;
+}
+
+const pokemonList = document.getElementById("pokemonList");
+
+pokeApi
+  .getPokemons()
+  .then((pokemons = []) => {
+    pokemonList.innerHTML+=pokemons.map(convertPokemonToLi).join('')
+  })
+  .catch((error) => console.log(error));
