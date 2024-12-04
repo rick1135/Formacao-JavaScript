3c5ee4dcf9600caff939831c7dd5afe0cbb4f27d
rick1135 <rick1135@hotmail.com>
2024-12-04 18:49:42 -0300

    studying

--- Desafio - Pokédex/assets/js/main.js
+++ Desafio - Pokédex/assets/js/main.js
@@ -1,8 +1,47 @@
-const offset = 0;
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const maxRecords = 151;
 const limit = 10;
-const url = 'https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}';
+let offset = 0;
 
-fetch(url)
-    .then(response => response.json())
-    .then(jsonBody => console.log(jsonBody))
-    .catch(error => console.log('Erro na requisição: ', error));
+function loadPokemonItens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons
+      .map(
+        (pokemon) => `
+              <li class="pokemon ${pokemon.type}">
+                  <span class="number">#${pokemon.number}</span>
+                  <span class="name">${pokemon.name}</span> 
+                  <div class="detail">
+                  <ol class="types">
+                      ${pokemon.types
+                        .map((type) => `<li class="type ${type}">${type}</li>`)
+                        .join("")}
+                  </ol>
+                  <img src="${pokemon.photo}">
+                <button class="details-button" onclick="showDetails(${
+                  pokemon.number
+                })">Detalhes</button>
+                  </div>
+              </li>
+          `
+      )
+      .join("");
+    pokemonList.innerHTML += newHtml;
+  });
+}
+
+
+loadPokemonItens(offset, limit);
+
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
+  const qtdRecord = offset + limit;
+  if (qtdRecord >= maxRecords) {
+    const newLimit = maxRecords - offset;
+    loadPokemonItens(offset, newLimit);
+    loadMoreButton.parentElement.removeChild(loadMoreButton);
+  } else {
+    loadPokemonItens(offset, limit);
+  }
+});
