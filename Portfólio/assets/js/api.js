202956a996b71e87b5711f1f04297cb3215b0cc9
rick1135 <rick1135@hotmail.com>
2024-12-07 17:40:24 -0300

    profile photo

--- Portfólio/assets/js/api.js
+++ Portfólio/assets/js/api.js
@@ -1,3 +1,5 @@
-function fetchProfileData(){   
-    const url = '';
+async function fetchProfileData(){   
+    const url = 'https://raw.githubusercontent.com/rick1135/Formacao-JavaScript/refs/heads/main/Portfólio/data/profile.json';
+    const fetching = await fetch(url);
+    return await fetching.json(); 
 }
---
+++ Portfólio/assets/js/main.js
@@ -0,0 +1,10 @@
+function updateProfileInfo(profileData) {
+    const photo = document.getElementById('profile.photo');
+    photo.src = profileData.photo;
+    photo.alt = profileData.name;
+}
+
+(async () => {
+    const profileData = await fetchProfileData();
+    updateProfileInfo(profileData);
+})()
