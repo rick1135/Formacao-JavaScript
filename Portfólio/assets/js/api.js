76307cac62940f301dc889dfc2c49f604551d0c9
Alberto Henrique Mauricio Ribeiro <55491414+rick1135@users.noreply.github.com>
2024-12-07 17:43:41 -0300

    Update api.js

--- Portfólio/assets/js/api.js
+++ Portfólio/assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData(){   
-    const url = 'https://avatars.githubusercontent.com/u/55491414?v=4';
+    const url = 'https://raw.githubusercontent.com/rick1135/Formacao-JavaScript/refs/heads/main/Portfólio/data/profile.json';
     const fetching = await fetch(url);
     return await fetching.json(); 
 }
